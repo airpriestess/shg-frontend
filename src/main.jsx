f4601e5ab1d180a9e855dac03dbcc7bcf644ab09
airpriestess <107564915+airpriestess@users.noreply.github.com>
2026-07-05 22:20:13 +0200

    Auth wiring (main.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><AuthProvider><App /></AuthProvider></React.StrictMode>
 )
